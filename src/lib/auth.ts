ef017178e21f77f8b0376b9831ea6de40f24a982
Nishat-Annan-Mim <nishat.annan.mim.2003@gmail.com>
2026-07-13 13:20:04 +0600

    first commit

---
+++ src/lib/auth.ts
@@ -0,0 +1,25 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGO_URI as string);
+const db = client.db();
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db),
+  emailAndPassword: {
+    enabled: true,
+    minPasswordLength: 6,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7, // 7 days
+    updateAge: 60 * 60 * 24, // refresh every 1 day
+  },
+  trustedOrigins: [process.env.NEXT_PUBLIC_API_URL as string],
+});
