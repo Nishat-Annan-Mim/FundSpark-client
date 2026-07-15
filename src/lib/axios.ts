93a13ffaf9e26345e64ed5703f5c6e7ae656ad25
Nishat-Annan-Mim <nishat.annan.mim.2003@gmail.com>
2026-07-15 22:00:09 +0600

    deploy problem

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,15 @@
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_API_URL,
+//   withCredentials: true,
+// });
+
+// export default axiosInstance;
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "/api/backend",
   withCredentials: true,
 });
 
